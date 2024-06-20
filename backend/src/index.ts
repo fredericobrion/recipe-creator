e30d660df32bd0d7df1bca709eb7d0483d7253d4
Frederico Brion <fredericoasbrion@gmail.com>
2024-06-20 16:07:01 -0300

    feat: initial settings

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, TypeScript with Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
